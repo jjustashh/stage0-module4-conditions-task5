b1f9223b17e0fa777ad5a83523ad815c4c1e0d8d
Donat <donatborkovskij@gmail.com>
2023-10-15 17:34:46 +0200

    commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String result;
+
+        switch (year % 4) {
+            case 0:
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        result = "leap";
+                    } else {
+                        result = "not leap";
+                    }
+                } else {
+                    result = "leap";
+                }
+                break;
+            default:
+                result = "not leap";
+                break;
+        }
+
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                System.out.println(31);
+                break;
+            case 2:
+                System.out.println(28);
+                break;
+            case 4, 11, 6, 9:
+                System.out.println(30);
+                break;
+            default:
+                System.out.println("wrong number!");
+        }
     }
 }
